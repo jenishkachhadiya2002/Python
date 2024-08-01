97ae19a181f026c86e5e2c54ea7c1e42b531d04a
jenishkachhadiya2002 <tg.jenishkachhadiya@gmail.com>
2024-08-01 11:04:01 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,59 @@
+    # Multiple variabel
+
+# X = Y = N = "google 12"
+
+# print(X,Y,N)
+
+
+
+
+X = [1,2,3,4,5]
+print(X)
+print(type(X))
+# print(X[4])
+# print(X[::-1])
+# print(X[3::-2])
+
+
+
+# Globel variable 
+
+# def Transglobe() :
+
+#     f = 12
+#     b = 1.2
+#     c = "Hello"
+#     D = [1,2,3,4,5]
+
+#     print(f,b,c,D)
+#     print(f+b)
+
+# Transglobe()
+
+
+
+# x = (1,2,3,4)
+
+# print(x)
+# print(type(x))
+# print(x[1])
+# print(x[::-1])
+# print(x[0:3])
+# print(x[0:-2])
+
+
+# Nested List
+
+# c = [(1,2,3,4,5,6),1,[1,2,3],"HELLO TG"]
+
+# print(c[-2])
+# print(type(c))
+
+
+
+# X = (1,2,3,4,5,6,7,8)
+
+# print(X[::-1])
+# print(X[-3:-5:-1])
+# print(X[:0])
+# print(X[1:8:1])
