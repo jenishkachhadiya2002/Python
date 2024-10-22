841fecf45ef63204a91c1bef557c3ca9485e0b8b
jenishkachhadiya2002 <tg.jenishkachhadiya@gmail.com>
2024-10-22 16:42:49 +0530

    Add files via upload

---
+++ FIVE_AD.py
@@ -0,0 +1,75 @@
+# .....INHARITANCE.....
+
+# class
+
+# class A:
+#     def hello1(self):
+#         print("right")
+#     def hello2(self):
+#         print("wrong")
+# class B(A):
+#     def feach1(self):
+#         print("hello 1")
+#     def feach2(self):
+#         print("hello 2")
+# class C(B):
+#     def feach3(self):
+#         print("hello 3")
+#     def feach4(self):
+#         print("hello 4")
+
+# b1=C()
+# b1.hello1()
+# b1.hello2()
+# b1.feach1()
+# b1.feach2()
+# b1.feach3()
+# b1.feach4()
+
+# .....ITRETOR.....
+
+# HELLO=[1,2,3,4,5,6]
+# its=iter(HELLO)
+
+# print(its.__next__())
+# print(its.__next__())
+# print(its.__next__())
+# print(its.__next__())
+# print(its.__next__())
+# print(next(its))
+
+# x=(1,2,3,4,5,6,7)
+# itss=iter(x)
+# print(itss.__next__())
+# print(itss.__next__())
+# print(itss.__next__())
+# print(itss.__next__())
+# print(itss.__next__())
+# print(next(itss))
+ 
+ 
+#.....GENERATORURS.....
+
+# def hello():
+#     yield "hello"
+
+# value=hello()
+# print(value.__next__())
+
+# .....PYTHON.....MATH.....
+
+# x=min(1,2,3,4,5)
+# print(x)
+
+# s=max(1,2,3,4,5)
+# print(s)
+
+# x=abs(-4.34)
+# print(x)
+
+# y=pow(2,3)
+# print(y)
+
+# import math
+# x=math.pi
+# print(x)
