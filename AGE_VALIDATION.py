79088c11db97a63abec417477c945aaeeb302f9b
jenishkachhadiya2002 <tg.jenishkachhadiya@gmail.com>
2024-11-23 14:53:24 +0530

    Update AGE_VALIDATION.py

--- AGE_VALIDATION.py
+++ AGE_VALIDATION.py
@@ -8,3 +8,15 @@ def calculateAge (Birthdate):
     return Age
 
 print(calculateAge(date(2002,8,21)),"year")
+
+
+
+
+age = int(input("Enter Age : "))
+
+if age <= 18==53:
+    status = "Eligible"
+else:
+    status = "Not Eligible"
+
+print("You are ", status, " for Vote.")
